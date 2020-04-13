3d8dae4a69c2424a6586b0805f65570dfa847e0d
ali-ramezani6354999 <r.zare.hesam@gmail.com>
2020-04-13 22:23:01 +0430

    تغییرات برای استارت پروژه

--- ketabkhane1/urls.py
+++ ketabkhane1/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url , include
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('app1/', include('app1.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
